0951416e6de6a321dac6a7735f230b7e0cb3a0b2
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-06-02 18:16:48 -0600

    about page update

--- src/routes/about/+layout.svelte
+++ src/routes/about/+layout.svelte
@@ -1,28 +1,24 @@
 <script lang="ts">
-
 </script>
 
-<div id="aboutWrapper" class="max-w-5xl m-auto my-8">
-	<div class="card variant-glass-surface p-4 relative h-96 overflow-hidden">
-		<section class="space-y-4">
-			<h1 class="text-9xl">
-				<span
-					class="bg-gradient-to-br variant-gradient-tertiary-secondary bg-clip-text text-transparent box-decoration-clone font-bold"
-				>
-					Dylan Smith
-				</span>
-			</h1>
+<div id="aboutWrapper" class="max-w-5xl m-auto my-8 space-y-4">
+	<div class="variant-glass card overflow-hidden">
+		<div
+			class="h-96 w-full overflow-hidden p-4"
+			style="background: url(/PortfolioPortrait.png) no-repeat right bottom; background-size: 24em;"
+		>
+			<section class="space-y-4">
+				<h1 class="text-9xl">
+					<span
+						class="bg-gradient-to-br variant-gradient-tertiary-secondary bg-clip-text text-transparent box-decoration-clone font-bold"
+					>
+						Dylan Smith
+					</span>
+				</h1>
 
-			<p class="font-mono font-bold text-3xl opacity-80">Web and App Developer // UX Designer</p>
-		</section>
-
-		<figure class="absolute bottom-0 right-0 z-10">
-			<img
-				src="/PortfolioPortrait.png"
-				alt="Portoflio Portrait"
-				class="h-96 w-auto drop-shadow-xl"
-			/>
-		</figure>
+				<p class="font-mono font-bold text-3xl opacity-80">Web and App Developer // UX Designer</p>
+			</section>
+		</div>
 	</div>
 
 	<slot />
