11902f20a3f2fc7547a43869e7b901c0b7b23836
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-06-13 18:08:21 -0600

    added stores and toasts

---
+++ src/lib/stores/index.ts
@@ -0,0 +1,20 @@
+import { localStorageStore } from '@skeletonlabs/skeleton'
+import type { Writable } from 'svelte/store'
+import { browser } from '$app/environment'
+
+const defaultValue = browser ? window.localStorage.getItem('draft') : ''
+
+const draft: Writable<string> = localStorageStore('draft', JSON.stringify(defaultValue))
+
+draft.subscribe((value) => {
+	if (value === 'undefined' || value === 'null') {
+		value = ''
+		return
+	}
+
+	if (browser) {
+		window.localStorage.setItem('draft', JSON.stringify(value))
+	}
+})
+
+export { draft }
--- src/routes/auth/admin/documents/create-document/+page.svelte
+++ src/routes/auth/admin/documents/create-document/+page.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
 	import SvelteHtmlEditor from '../../../../../components/SvelteHTMLEditor/SvelteHTMLEditor.svelte'
+	import { draft } from '$lib/stores'
 
-	let value: string
+	// This is a custom Svelte component that I made with assistance of GitHub Copilot.
+	// All I need is a HTML editor that is simple and has widgets for common article UI
+	// elements. I don't need a full-fledged WYSIWYG editor like TinyMCE or CKEditor--especially
+	// since I'm using SvelteKit and not a framework like Vue or React.
+
+	let value: string = !$draft ? '' : JSON.parse($draft)
 	let mouseDown: boolean = false
 	let left: any
 	let parent: HTMLElement
+	let minWidth: number = 500
+	let previewPane: HTMLElement
+	let editorPane: HTMLTextAreaElement
+
+	onMount(() => {
+		console.log(value, $draft)
+
+		const interval = setInterval(() => {
+			console.log('Auto Save...')
+		}, 1000 * 60)
+
+		return () => clearInterval(interval)
+	})
 </script>
 
 <section
@@ -14,6 +34,8 @@
 	on:mousemove={(e) => {
 		if (!mouseDown) return
 		const parentRect = parent.getBoundingClientRect()
+		if (e.pageX - parentRect.left < minWidth) return
+		if (e.pageX - parentRect.left > parentRect.width - minWidth) return
 		left.style.width = `${e.pageX - parentRect.left}px`
 	}}
 	on:mouseup={() => {
@@ -23,21 +45,31 @@
 	<SvelteHtmlEditor
 		bind:left
 		bind:value
+		bind:editorPane
 		container="w-1/2 flex flex-col"
 		body="w-full h-full rounded-t-none rounded-r-none"
-		widgetsContainer="card p-1 rounded-b-none rounded-r-none"
+		widgetsContainer="card p-2 rounded-b-none rounded-r-none"
 	/>
 
 	<div
-		class="split__bar w-2 card rounded-none variant-filled-surface flex items-center justify-center cursor-col-resize"
-		on:mousedown={(event) => {
+		class="split__bar w-2 card hover:bg-surface-900 rounded-none variant-filled-surface flex items-center justify-center cursor-col-resize"
+		on:mousedown={() => {
 			mouseDown = true
 		}}
+		on:dblclick={() => {
+			left.style.width = '50%'
+		}}
 	>
-		|
+		<span class="select-none"> | </span>
 	</div>
 
-	<div class="flex-1 card rounded-l-none h-full break-all">
-		{@html value}
+	<div bind:this={previewPane} class="flex-1 card rounded-l-none h-full break-all overflow-y-auto">
+		{#if !value}
+			<div class="flex items-center justify-center h-full">
+				<p class="text-2xl text-gray-400">Nothing to preview</p>
+			</div>
+		{:else}
+			{@html value}
+		{/if}
 	</div>
 </section>
