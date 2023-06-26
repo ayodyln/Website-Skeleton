1ab12df702a2e39700dd0221b4d48e90ebef04bf
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-06-26 14:51:43 -0600

    article solution done

--- src/routes/blog/[blog]/+page.svelte
+++ src/routes/blog/[blog]/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { browser } from '$app/environment'
 	import { onMount } from 'svelte'
+	import { TableOfContents } from '@skeletonlabs/skeleton'
 
-	export let data: any
+	export let data
 
 	$: readTimeVal = NaN
 
@@ -17,7 +18,6 @@
 	}
 
 	onMount(() => {
-		console.log(data)
 		readTime()
 	})
 </script>
@@ -32,32 +32,46 @@
 <article id="blog_post" class="m-auto my-4 flex w-full max-w-5xl flex-col gap-4">
 	<!-- Title -->
 	<hgroup>
-		<h1 class="h1 mb-1 font-mono font-bold tracking-wide">{data.meta.title}</h1>
-		<span class="text-lg">
-			{new Intl.DateTimeFormat('en-US', {
-				year: 'numeric',
-				month: 'long',
-				day: 'numeric'
-			}).format(new Date(data.meta.date))}
+		<h1 class="h1 mb-2 font-mono font-bold tracking-wide">{data.meta.title}</h1>
+		<div class="flex items-center justify-between">
+			<span class="text-lg">
+				{new Intl.DateTimeFormat('en-US', {
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric'
+				}).format(new Date(data.meta.date))}
 
-			{@html `&#x2014;`}
+				{@html `&#x2014;`}
 
-			{#if readTimeVal}
-				{readTimeVal} Minute Read
-			{/if}
-		</span>
-		<!-- Tags -->
+				{#if readTimeVal}
+					{readTimeVal} Minute Read
+				{/if}
+			</span>
+
+			<!-- Tags -->
+			<span class="flex flex-wrap gap-2">
+				{#each data.meta.categories as tag}
+					<span class="badge variant-soft">{tag}</span>
+				{/each}
+			</span>
+		</div>
 	</hgroup>
 
 	<!-- Article Hero -->
 	<div
 		id="article_hero"
 		style={`background-image: url(${data.meta.hero_image});`}
-		class="card h-96 w-full"
+		class="card h-96 w-full shadow-lg"
 	/>
 
 	<!-- Post -->
-	<div id="md⬇️">
-		<svelte:component this={data.content} />
-	</div>
+	<section class="relative flex w-full max-w-4xl gap-4">
+		<div class="card sticky left-0 top-4 h-fit w-2/6 p-2">
+			<TableOfContents target="#blog_post" width="w-full" label="Blog Content" text="truncate" />
+		</div>
+
+		<div id="md⬇️" class="w-5/6 space-y-4">
+			<svelte:component this={data.content} />
+		</div>
+	</section>
 </article>
