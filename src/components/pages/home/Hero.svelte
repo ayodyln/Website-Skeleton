4c7074dcb4ea0447e871be51b4e0b9eb103bdb97
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-06-27 09:59:05 -0600

    landing  page

--- src/components/pages/home/Hero.svelte
+++ src/components/pages/home/Hero.svelte
@@ -1,5 +1,5 @@
 <div class="h-[85vh] flex flex-col items-center">
-	<div class="max-w-5xl m-auto space-y-4">
+	<div class="max-w-5xl w-full m-auto space-y-4">
 		<h1 class="text-9xl">
 			<span
 				class="font-mono bg-gradient-to-br variant-gradient-tertiary-secondary bg-clip-text text-transparent box-decoration-clone font-bold"
@@ -10,7 +10,7 @@
 
 		<p class="font-mono font-bold text-3xl">Web and App Developer // UX Designer</p>
 
-		<p class="text-xl">
+		<p class="text-xl max-w-3xl">
 			Currently attending university for my craft and have 3 plus years of experience from my
 			studies. I love creating applications from ideas and bringing beautiful user interfaces to
 			life.
